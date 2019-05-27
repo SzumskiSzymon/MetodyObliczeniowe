9a42e492f47c6ae686e11c55b6bd01c6a1139add
SzumskiSzymon <47870219+SzumskiSzymon@users.noreply.github.com>
2019-05-27 08:30:47 +0200

    Add files via upload

---
+++ rownaniaNieliniowe/MainSieczne.java
@@ -0,0 +1,85 @@
+package rownaniaNieliniowe;
+
+public class MainSieczne {
+
+    public static void main(String[] args){
+        double a = 1;
+        double b = 6;
+        double epsilon = 0.05;
+
+        System.out.println(sieczna(a,b,epsilon,3));
+        //System.out.println(x(a,b,0,false));
+
+        //System.out.println(sieczna(a,b,epsilon));
+    }
+
+    private static double sieczna(double a,double b,double epsilon, int i){
+        double wynik = 0;
+
+        if(mainWarunek(a,b)) {
+            if ((Math.abs(funkcja(x(a, b, i))) <= epsilon) || (Math.abs(x(a, b, i+1)-x(a, b, i))) <= epsilon) {
+                wynik = x(a, b, i);
+            }
+        }
+
+        return wynik;
+    }
+
+
+    // funkcje do wyliczania x
+
+        // true -> x0 = b, nieruchome a
+        // false -> x0 = a, nieruchome b
+
+
+    private static double x0(double a,double b){
+        double x = 0;
+
+        if(((funkcja(a)>0 && pochodnaII()>0) || (funkcja(a)<0 && pochodnaII()<0))){
+            x = a;
+        }else if(((funkcja(b)>0 && pochodnaII()>0) || (funkcja(b)<0 && pochodnaII()<0))){
+            x = b;
+        }
+
+        return x;
+    }
+
+
+    private static double x(double a,double b,int i){
+        if(i==0){
+            if(x0(a,b) == b) {
+                return x0(a, b) - (funkcja(x0(a, b)) / (funkcja(x0(a, b)) - funkcja(a))) * (x0(a, b) - a);
+            }else if(x0(a,b) == a){
+                return x0(a, b) - (funkcja(x0(a, b)) / (funkcja(x0(a, b)) - funkcja(a))) * (b - x0(a, b));
+            }else {
+                return 0;
+            }
+        }else {
+            if(x0(a,b) == b){
+                return x(a,b,i-1) - (funkcja(x(a,b,i-1)) / (funkcja(x(a,b,i-1)) - funkcja(a))) * (x(a,b,i-1) - a) ;
+            }else if(x0(a,b) == a){
+                return x(a, b, i - 1) - (funkcja(x(a, b, i - 1)) / (funkcja(x(a, b, i - 1)) - funkcja(a))) * (b - x(a, b, i - 1));
+            }else {
+                return 0;
+            }
+        }
+    }
+
+    // metody do obliczania funkcji
+
+    private static double funkcja(double x){
+        return (-24)*Math.pow(x,2) -10*x + 154;
+    }
+    private static double pochodna(double x){
+        return (-48)*x - 10;
+    }
+    private static double pochodnaII(){
+        return -48;
+    }
+
+    // metody na warynki
+
+    private static boolean mainWarunek(double a,double b){
+        return funkcja(a)*funkcja(b)<0;
+    }
+}
